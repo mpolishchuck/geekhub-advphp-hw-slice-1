56055c6716b17f1e669e179f9a23d239b9bcab45
Maxim Polishchuck <mpolishchuck@gmail.com>
2013-10-19 13:06:23 +0300

    Added PetCreature, SphericalCat and demo2 cli script

---
+++ cli/demo2.php
@@ -0,0 +1,35 @@
+<?php
+include '../src/AutoLoader.php';
+AutoLoader::register();
+
+$animal = new \Home\SphericalCat('Fluffy');
+
+$i = 0;
+while (true) {
+    printf(
+        "%3d. %s - Health: %.1f; Bellyful: %.1f; Energy: %.1f; Mood: %.1f - %s, %s, %s\n",
+        ++$i,
+        $animal->getName(),
+        $animal->getHealth(),
+        $animal->getBellyful(),
+        $animal->getEnergy(),
+        $animal->getMood(),
+        $animal->isSleeping() ? 'Sleeping' : 'Active',
+        $animal->isPlaying() ? 'Playing' : 'Idle',
+        $animal->isAlive() ? 'Still alive' : 'Dead'
+    );
+    if ($animal->isDead())
+        break;
+    if (($i % 15 == 0) && !$animal->isSleeping()) {
+        echo "Playing with {$animal->getName()}\n";
+        $animal->play(5);
+    }
+    if ($i % 21 == 0) {
+        echo "Feeding {$animal->getName()}\n";
+        $animal->feed(30);
+    }
+    $animal->think();
+    sleep(1);
+}
+
+echo "Working cycle complete\n";
