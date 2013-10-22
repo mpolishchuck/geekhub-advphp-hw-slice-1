83d3d5813e5ea767e07ae30799fc2f4a7491b2a9
Maxim Polishchuck <mpolishchuck@gmail.com>
2013-10-23 01:01:29 +0300

    Added PHPDoc blocks for all interfaces

--- src/Universe/CreatureInterface.php
+++ src/Universe/CreatureInterface.php
@@ -2,13 +2,40 @@
 
 namespace Universe;
 
+/**
+ * Interface CreatureInterface
+ * @package Universe
+ *
+ * Interface for creature
+ * Creature can have following attributes:
+ *  - health
+ *  - bellyful
+ *  - energy
+ * Also every creature can sleep
+ */
 interface CreatureInterface extends LifeFormInterface
 {
+    /**
+     * Get current health of the creature
+     * @return float current health of the creature
+     */
     public function getHealth();
 
+    /**
+     * Get current bellyful of the creature
+     * @return float current bellyful of the creature
+     */
     public function getBellyful();
 
+    /**
+     * Get current energy of the creature
+     * @return mixed current energy of the creature
+     */
     public function getEnergy();
 
+    /**
+     * Is creature sleeping now?
+     * @return bool
+     */
     public function isSleeping();
 }
--- src/Universe/ThingInterface.php
+++ src/Universe/ThingInterface.php
@@ -2,7 +2,19 @@
 
 namespace Universe;
 
+/**
+ * Interface ThingInterface
+ * @package Universe
+ *
+ * Interface for universe thing
+ * Every thing in the universe changes according to time flow
+ */
 interface ThingInterface
 {
+    /**
+     * "Thinker" of the universe thing
+     * Do process of thing
+     * Simulates time flow, changes internal variables according to time flow
+     */
     public function think();
 }
